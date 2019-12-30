8c37e875d1e4b017ccb34c4b3defd4baf7adf9dc
Lyle Scott, III <lyle@ls3.io>
2019-12-29 23:54:47 -0500

    deploy changes

--- deploy.sh
+++ deploy.sh
@@ -4,10 +4,12 @@ set -ex
 hugo
 
 # Minimize CSS.
-#cp static/css/bootstrap.min.css static/css/styles.css
-#cat static/css/custom.css >> /tmp/combined.css
-#curl -X POST -s --data-urlencode 'input@/tmp/combined.css' https://cssminifier.com/raw \
-#    >> static/css/styles.css
+cp static/css/pure.css /tmp/combined.css
+cat static/css/styles.css >> /tmp/combined.css
+curl -X POST -s --data-urlencode 'input@/tmp/combined.css' https://cssminifier.com/raw \
+    > public/css/styles.css
+
+# Minimize HTML
 html-minifier --input-dir ./public --output-dir ./public --collapse-whitespace --file-ext html
 
 # Sync files to S3.
