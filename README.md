fc45795b70f40f514b77c4c6ae0d86e2696be79d
Lyle Scott, III <lyle@ls3.io>
2019-12-29 22:45:44 -0500

    redo some nonsense

--- README.md
+++ README.md
@@ -3,6 +3,6 @@ Site code for http://ls3.io
 I redesign my site every few years and this is a manifestation of that at some point. I don't put
 too much effort into it, so bare with me.
 
-Uses Hugo and it's hosted on AWS S3 served via Cloud Front (see in the [/infra](/infra) directory).
+Uses Hugo and it's hosted on AWS S3 served via Cloud Front (see in the [infra](infra) directory).
 
 Deploys are pretty lame and done via [deploy.sh](deploy.sh)
---
+++ deploy.sh
@@ -0,0 +1,17 @@
+set -ex
+
+# Build site and create ./public
+hugo
+
+# Minimize CSS.
+#cp static/css/bootstrap.min.css static/css/styles.css
+#cat static/css/custom.css >> /tmp/combined.css
+#curl -X POST -s --data-urlencode 'input@/tmp/combined.css' https://cssminifier.com/raw \
+#    >> static/css/styles.css
+#html-minifier --input-dir ./public --output-dir ./public --collapse-whitespace --file-ext html
+
+# Sync files to S3.
+aws s3 sync \
+    --quiet \
+    --delete \
+    public/ s3://ls3.io
